b6edf644b5c44f8bb61cd62b5f00dbaaf6b899e2
UTKARSH SINGH <usjadon19@gmail.com>
2024-02-25 19:07:40 +0530

    firebase integration

--- lib/init.dart
+++ lib/init.dart
@@ -1,3 +1,5 @@
+import 'package:dhatnoon/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_displaymode/flutter_displaymode.dart';
 import 'package:platform_info/platform_info.dart';
@@ -5,6 +7,10 @@ import 'package:platform_info/platform_info.dart';
 ///This function is used for setting up default orientation,
 ///display refresh rate, hide keyboard etc system services.
 Future<void> init() async {
+  await Firebase.initializeApp(
+    name: 'Dhatnoon',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
